596a8d308005b8fd9473f10252e875bd45121214
aidanoneill <agoneill@tcd.ie>
2015-03-17 18:49:29 +0000

    1

--- R/plotWordcloud.R
+++ R/plotWordcloud.R
@@ -4,7 +4,6 @@ plotWordcloud <-
     require("wordcloud")
     
     totals = apply(dataM, 2, sum)
-    return(wordcloud(words = getTerms(data), freq = totals, max.words=50,
-              random.order=FALSE, random.color = TRUE, colors=brewer.pal(n=9,name="Set1"))
+    return(wordcloud(words = getTerms(data), freq = totals, max.words=50, random.order=FALSE, random.color = TRUE, colors=brewer.pal(n=9,name="Set1"))
     
   }
